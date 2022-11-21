3392ece49757aef72683d1ca893b5e0c3dcf7ed5
emioiso <jijjjiijiijijiji@gmail.com>
2022-11-21 23:38:03 +0900

    A - ハンドルネーム

---
+++ abc010/a/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int	main(void)
+{
+	char str[11];
+	char p[] = "pp";
+
+	scanf("%s",str);
+	// printf("%s\n",str);
+	// printf("%s\n",p);
+
+	printf("%s%s\n",str,p);
+
+	return (0);
+}
