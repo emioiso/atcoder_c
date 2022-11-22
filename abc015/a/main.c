9fc9e180b6e51d77831f5256134dcb27d31834a9
emioiso <jijjjiijiijijiji@gmail.com>
2022-11-22 17:46:19 +0900

    A - 高橋くんの研修

---
+++ abc015/a/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+int	main(void)
+{
+	char	A[51];
+	char	B[51];
+
+	scanf("%s%s",A,B);
+	// printf("%s\n%s\n",A,B);
+
+	// printf("%lu\n", strlen(A));
+	// printf("%lu\n", strlen(B));
+
+	if(strlen(A) > strlen(B))
+	{
+		printf("%s\n", A);
+	} else {
+		printf("%s\n", B);
+	}
+
+	return (0);
+}
