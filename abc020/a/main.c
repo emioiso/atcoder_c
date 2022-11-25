2d33aa727e9ceaafdbce986e43568e90a6403546
emioiso <jijjjiijiijijiji@gmail.com>
2022-11-25 15:29:53 +0900

    A - クイズ

---
+++ abc020/a/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int	main(void)
+{
+	int q;
+
+	scanf("%d",&q);
+	// printf("%d\n",q);
+
+	if(q == 1){
+		printf("%s\n","ABC");
+	} else {
+		printf("%s\n","chokudai");
+	}
+
+	return (0);
+}
