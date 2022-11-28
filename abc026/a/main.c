93451003c136e1a237af5e1797f40105436069f7
emioiso <jijjjiijiijijiji@gmail.com>
2022-11-28 14:40:38 +0900

    A - 掛け算の最大値

---
+++ abc026/a/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int	main(void)
+{
+	int a;
+	int b = 0;
+
+	scanf("%d",&a);
+	// printf("%d\n",a);
+
+	b = a / 2;
+	// printf("%d\n", b);
+	printf("%d\n", b * b);
+
+	return (0);
+}
