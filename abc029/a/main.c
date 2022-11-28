68ded466f1c35bad351abb47c46f4f84065919ff
emioiso <jijjjiijiijijiji@gmail.com>
2022-11-28 17:34:08 +0900

    A - 複数形

---
+++ abc029/a/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int	main(void)
+{
+	char w[11];
+	char s[2] = "s";
+
+	scanf("%s",w);
+	// printf("%s\n",w);
+	// printf("%s\n",s);
+
+	printf("%s%s\n", w,s);
+
+	return (0);
+}
