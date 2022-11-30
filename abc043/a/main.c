47a4063781c0bb04224f40a5ce98888fd09cd680
emioiso <jijjjiijiijijiji@gmail.com>
2022-11-30 20:31:20 +0900

    [!]A - キャンディーとN人の子供イージー

---
+++ abc043/a/main.c
@@ -0,0 +1,34 @@
+//写経#35854653
+#include <stdio.h>
+
+int main()
+{
+  int n;
+  scanf("%d", &n);
+//   printf("%d\n", n);
+
+  int a = 0;
+  for (int i = 1; i <= n; i++) {
+    a+=i;		//ここがわからなかった
+  }
+  printf("%d\n", a);
+}
+
+
+// #include <stdio.h>
+
+// int	main(void)
+// {
+// 	int n;
+// 	int i;
+
+// 	scanf("%d",&n);
+// 	// printf("%d\n",n);
+// 	for (i = 1; i <= n; i++)
+// 	{
+// 		printf("%d\n", n + i);
+// 	}
+
+
+// 	return (0);
+// }
