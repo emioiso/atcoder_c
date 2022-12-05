b6a7bfc0269a686f5f8d7bc291189f86e6906f7c
emioiso <jijjjiijiijijiji@gmail.com>
2022-12-05 13:20:10 +0900

    A - K-City

---
+++ abc069/a/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int	main(void)
+{
+	int n;
+	int m;
+
+	scanf("%d%d",&n,&m);
+	// printf("%d %d\n",n,m);
+
+	int n1 = n - 1;
+	int m1 = m - 1;
+
+	printf("%d\n", n1 * m1);
+
+	return (0);
+}
