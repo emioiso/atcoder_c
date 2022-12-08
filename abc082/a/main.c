bda537691c7454203a6022111737b98c756ed780
emioiso <jijjjiijiijijiji@gmail.com>
2022-12-08 19:37:44 +0900

    A - Round Up the Mean

---
+++ abc082/a/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int	main(void)
+{
+	int a;
+	int b;
+
+	scanf("%d%d", &a, &b);
+	// printf("%d %d\n",a,b);
+
+	int ave = (a + b + 1) / 2;	//+1をすると切り上げられた数字になる。
+	printf("%d\n",ave);
+
+	return (0);
+}
