8c59b903edbfeeb8e4c265781988220662343bb6
emioiso <jijjjiijiijijiji@gmail.com>
2022-12-11 16:43:40 +0900

    A - ABD

---
+++ abc099/a/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int	main(void)
+{
+	int n;
+
+	scanf("%d", &n);
+	// printf("%d\n",n);
+
+	if(n < 1000){
+		printf("ABC\n");
+	} else {
+		printf("ABD\n");
+	}
+
+	return (0);
+}
