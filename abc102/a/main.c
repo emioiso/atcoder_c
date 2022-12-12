7044ccf9e6a91452a917e1390dcdb8654b683471
emioiso <jijjjiijiijijiji@gmail.com>
2022-12-12 19:37:53 +0900

    A - Multiple of 2 and N

---
+++ abc102/a/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int	main(void)
+{
+	int n;
+
+	scanf("%d", &n);
+	// printf("%d\n",n);
+
+	if((n % 2) != 0){
+		printf("%d\n",n * 2);
+	} else {
+		printf("%d\n",n);
+	}
+
+	return (0);
+}
