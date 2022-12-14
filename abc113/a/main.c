a800a516ce419171b0c936d49c5dda88fea42aab
emioiso <jijjjiijiijijiji@gmail.com>
2022-12-15 01:05:18 +0900

    A - Discount Fare

---
+++ abc113/a/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int	main(void)
+{
+	int x;
+	int y;
+
+	scanf("%d%d",&x,&y);
+	// printf("%d %d\n",x,y);
+
+	int y2 = y / 2;
+	// printf("%d",y2);
+
+	printf("%d\n",x + y2);
+	return (0);
+}
