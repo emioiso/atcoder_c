b45e2aa9f78b9872dc996b1d55e404f80688b400
emioiso <jijjjiijiijijiji@gmail.com>
2022-12-19 23:47:44 +0900

    A - Rounding

---
+++ abc130/a/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int	main(void)
+{
+
+	int	x;
+	int	a;
+
+	scanf("%d %d",&x,&a);
+	// printf("%d %d\n",x,a);
+
+	if(x < a){
+		printf("0\n");
+	} else {
+		printf("10\n");
+	}
+
+	return (0);
+}
