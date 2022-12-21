165366a4a7016424febea0684f72d753cea2cc00
emioiso <jijjjiijiijijiji@gmail.com>
2022-12-21 14:06:58 +0900

    [!]A - Garden

---
+++ abc106/a/main.c
@@ -0,0 +1,15 @@
+// 解説　公式が分からなくて解けなかった問題。
+
+#include <stdio.h>
+
+int	main(void)
+{
+	int	a;
+	int	b;
+
+	scanf("%d%d",&a,&b);
+	// printf("%d %d\n",a,b);
+	printf("%d\n",(a - 1) * (b - 1));
+
+	return (0);
+}
