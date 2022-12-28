bfd4523ece800c5047719c0d26fa57ec4de2171a
emioiso <jijjjiijiijijiji@gmail.com>
2022-12-28 15:09:36 +0900

    A - AC or WA

---
+++ abc152/a/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int	main(void)
+{
+
+	int	n;
+	int	m;
+
+	scanf("%d%d",&n,&m);
+	// printf("%d %d\n",n,m);
+
+	if(n == m){
+		printf("Yes\n");
+	} else {
+		printf("No\n");
+	}
+	return (0);
+}
