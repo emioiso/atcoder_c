6eccf41680edd1bc16af180a0cd686d8002168ce
emioiso <jijjjiijiijijiji@gmail.com>
2023-02-15 13:55:05 +0900

    A - Tiny Arithmetic Sequence

---
+++ abc201/a/main.c
@@ -0,0 +1,36 @@
+// #include <stdio.h>
+// int	main(void)
+// {
+// 	int	a1;
+// 	int	a2;
+// 	int	a3;
+
+// 	scanf("%d%d%d",&a1,&a2,&a3);
+// 	// printf("%d %d %d\n",a1,a2,a3);
+
+// 	if(a1 + a3 == 2 * a2){
+// 		printf("Yes\n");
+// 	} else {
+// 		printf("No\n");
+// 	}
+// 	return (0);
+// }
+
+
+// #include <stdio.h>
+// int	main(void)
+// {
+// 	int	a1;
+// 	int	a2;
+// 	int	a3;
+
+// 	scanf("%d%d%d",&a1,&a2,&a3);
+// 	// printf("%d %d %d\n",a1,a2,a3);
+
+// 	if(a1 + a2 == 2 * a3 || a2 + a3 == 2 * a1 || a1 + a3 == 2 * a2){
+// 		printf("Yes\n");
+// 	} else {
+// 		printf("No\n");
+// 	}
+// 	return (0);
+// }
