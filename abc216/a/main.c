9faedd87391ab5e10e721e0f9e44a275f324d064
emioiso <jijjjiijiijijiji@gmail.com>
2023-02-18 22:25:43 +0900

    [!]A - Signed Difficulty

---
+++ abc216/a/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int	main(void)
+{
+	int	x;
+	int	y;
+
+	// printf("%d%d\n",x,y);
+
+	if (y < 3){
+		printf("%d-\n", x);
+	} else if (y < 7){
+		printf("%d\n", x);
+	} else {
+		printf("%d+\n", x);
+	}
+	return (0);
+}
