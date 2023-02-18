68fc06a451691c286d882c7933138f016fe3d9db
emioiso <jijjjiijiijijiji@gmail.com>
2023-02-18 22:53:15 +0900

    B - qwerty

---
+++ abc218/b/main.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+int	main(void)
+{
+	int p[27];
+
+	for (int i = 0; i < 26; i++)
+	{
+		scanf("%d",&p[i]);
+		// printf("%d ",p[i]);
+
+		if(p[i] == 1){
+			printf("a");
+		} else if(p[i] == 2){
+			printf("b");
+		} else if(p[i] == 3){
+			printf("c");
+		} else if(p[i] == 4){
+			printf("d");
+		} else if(p[i] == 5){
+			printf("e");
+		} else if(p[i] == 6){
+			printf("f");
+		} else if(p[i] == 7){
+			printf("g");
+		} else if(p[i] == 8){
+			printf("h");
+		} else if(p[i] == 9){
+			printf("i");
+		} else if(p[i] == 10){
+			printf("j");
+		} else if(p[i] == 11){
+			printf("k");
+		} else if(p[i] == 12){
+			printf("l");
+		} else if(p[i] == 13){
+			printf("m");
+		} else if(p[i] == 14){
+			printf("n");
+		} else if(p[i] == 15){
+			printf("o");
+		} else if(p[i] == 16){
+			printf("p");
+		} else if(p[i] == 17){
+			printf("q");
+		} else if(p[i] == 18){
+			printf("r");
+		} else if(p[i] == 19){
+			printf("s");
+		} else if(p[i] == 20){
+			printf("t");
+		} else if(p[i] == 21){
+			printf("u");
+		} else if(p[i] == 22){
+			printf("v");
+		} else if(p[i] == 23){
+			printf("w");
+		} else if(p[i] == 24){
+			printf("x");
+		} else if(p[i] == 25){
+			printf("y");
+		} else if(p[i] == 26){
+			printf("z");
+		}
+	}
+	printf("\n");
+	return (0);
+}
