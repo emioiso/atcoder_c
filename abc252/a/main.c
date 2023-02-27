5e4661497b3b15eea3a7a461f61dee128c221e0e
emioiso <jijjjiijiijijiji@gmail.com>
2023-02-27 17:44:16 +0900

    A - ASCII code

---
+++ abc252/a/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int	main(void)
+{
+	int	 n;
+
+	scanf("%d",&n);
+	// printf("%d\n",n);
+	// printf("%d\n",n);
+
+	if('a' <= n && n <= 'z'){
+		printf("%c\n", n);
+	}
+	return (0);
+}
