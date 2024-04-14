88e33c60216228d6bbee2eda3028c8e4ea66c0e1
emioiso <jijjjiijiijijiji@gmail.com>
2024-04-14 17:06:32 +0900

    A - Long Loong

---
+++ abc336/a/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void){
+   int n;
+   scanf("%d",&n);
+   printf("L");
+    for(int i = 0; i < n; i++){
+        if(i < n){
+            printf("o");
+        }
+    }
+    printf("ng");
+}
